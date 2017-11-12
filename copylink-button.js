49a780f9d3e15e208e6bc83527ca02edf5e7bebb
YeomanYe <ming13821007683@gmail.com>
2017-11-12 12:28:34 +0800

    update copylink-button

--- copylink-button.js
+++ copylink-button.js
@@ -4,19 +4,33 @@
 // @version      0.1
 // @description  电影天堂链接复制按钮
 // @author       Ming Ye
-// @match        http://www.ygdy8.com/html/gndy/jddy/*/*
-// @match        http://www.dytt8.net/html/gndy/jddy/*/*
-// @require      https://greasyfork.org/scripts/34143-debug/code/debug.js?version=223801
+// @match        http://www.ygdy8.com/html/gndy/*/*/*
+// @include      http://www.ygdy8.net/html/gndy/*/*/*
+// @match        http://www.dytt8.net/html/gndy/*/*/*
+// @include      http://www.dy2018.com/*/*
+// @include      http://www.dygod.net/html/gndy/*/*/*
+// @require      https://greasyfork.org/scripts/34143-debug/code/debug.js?version=229638
 // @require      http://cdn.bootcss.com/jquery/1.8.3/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var $zoom = $('#Zoom');
-    var $buttonContainer = $zoom.find('strong:contains("【下载地址】")'),
-        $td = $zoom.find('table td'),
-        $linkButton = $('<a>复制</a>');
-    $td.append($linkButton);
-    console.log($buttonContainer,$td);
+    var log = consoleFactory('复制链接:','log',null,false);
+    var $zoom = $('#Zoom'),
+        $container = $zoom.find('table td'), //button容器
+        $a = $container.find('a'), //超链接元素
+        $btnTemplate = $('<button>复制</button>'); //按钮模板
+    $btnTemplate.attr("data-clipboard-action","copy");
+    log('循环外');
+    for(var i=0,len=$a.length;i<len;i++){
+        var $cloneBtn = $btnTemplate.clone().attr({
+            'data-clipboard-text':$a.eq(i).text(),
+            'class':'btn'
+        });
+        $container.eq(i).append($cloneBtn);
+        log('循环内');
+    }
+    new Clipboard('.btn');
 })();
--- readme.md
+++ readme.md
@@ -3,8 +3,20 @@
 
 ## comic-conv-script
 点击漫画图片翻页的脚本
+[脚本地址](https://greasyfork.org/zh-CN/scripts/33966-%E6%BC%AB%E7%94%BB%E7%BF%BB%E9%A1%B5%E8%84%9A%E6%9C%AC)
 
 支持站点：
 [kuku动漫](http://comic.kukudm.com/)
 
-[脚本地址](https://greasyfork.org/zh-CN/scripts/33966-%E6%BC%AB%E7%94%BB%E7%BF%BB%E9%A1%B5%E8%84%9A%E6%9C%AC)
+
+### copylink-button
+给电影天堂超链接设置一个复制链接的按钮，如果迅雷等下载器已经启动，会嗅探到下载链接并建立下载任务。
+[脚本地址]()
+
+支持站点：
+[电影天堂-迅雷电影dytt8](http://www.dytt8.net/)
+[电影天堂dy2018](http://www.dy2018.com/)
+[电影天堂dygod](http://www.dygod.com/)
+[阳光电影ygdy8.com](http://www.ygdy8.com/)
+[阳光电影ygdy8.net](http://www.ygdy8.net/)
+
