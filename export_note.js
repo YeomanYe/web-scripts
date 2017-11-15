4936210ed1a0f76a4d6403d33ac1ca80860edb0c
YeomanYe <ming13821007683@gmail.com>
2017-11-15 22:44:22 +0800

    update export_note

--- export_note.js
+++ export_note.js
@@ -15,53 +15,61 @@
 (function() {
     'use strict';
 
-    var log = consoleFactory('导出笔记脚本:','log',null,true),
-        warn = consoleFactory('导出笔记脚本:','warn',null,true);
+    var log = consoleFactory('导出笔记脚本:', 'log', null, true),
+        warn = consoleFactory('导出笔记脚本:', 'warn', null, true);
     log(saveAs);
     var $btnContainer = $('#main .course-info-main .content-wrap .mod-tab-menu .course-menu');
-    var $collectBtn = $('<li><a style="color:orange;cursor:pointer;">导出笔记</a></li>'),//采集按钮
-        $praiseBtn = $('<li><a style="color:orange;cursor:pointer;">赞</a></li>'),//采集按钮
+    var $collectBtn = $('<li><a style="color:orange;cursor:pointer;">导出笔记</a></li>'), //采集按钮
+        $praiseBtn = $('<li><a style="color:orange;cursor:pointer;">赞</a></li>'), //采集按钮
         courseId = window.location.pathname.split("/")[2], //课程ID
-        pageNum = 1,//笔记页码
+        pageNum = 1, //笔记页码
         $iframe = $("<iframe hidden></iframe>"),
-        baseUrl = 'http://'+window.location.hostname+'/note/'+courseId+'?sort=hot&page=';
-    log('课程ID',courseId);
-    $iframe[0].onload = function(e){
-        log(e,this);
+        isPause = localStorage.getItem('collectPause') || false,//标志是否停止采集
+        baseUrl = 'http://' + window.location.hostname + '/note/' + courseId + '?sort=hot&page=';
+    log('课程ID', courseId);
+
+    function iframeLoadHandler(e) {
+        log(e, this);
         var $contDoc = $(this.contentDocument);
         var $collectI = $contDoc.find('#js-note-container .Jcollect i');
         var flag = false; //笔记成功加载并处理后，才处理下一页
         warn($collectI);
         var notes = JSON.parse(localStorage.getItem('notes'));
-        for(var i=0,len=$collectI.length;i<len;i++){
+        for (var i = 0, len = $collectI.length; i < len; i++) {
             notes = notes || [];
             flag = true;
-            if($collectI.eq(i).text()=='已采集'){
+            if ($collectI.eq(i).text() == '已采集') {
                 var $content = $collectI.eq(i).parents('li.post-row'),
                     noteStr = $content.find('.js-notelist-content .autowrap')[0].innerText,
                     user = $content.find('.bd .tit')[0].innerText,
-                    time = $content.find('.footer .timeago')[0].innerText.replace('时间：',''),
-                    chapter = $content.find('.footer .from')[0].innerText.replace('源自：',''),
+                    time = $content.find('.footer .timeago')[0].innerText.replace('时间：', ''),
+                    chapter = $content.find('.footer .from')[0].innerText.replace('源自：', ''),
                     data = {
-                        note:noteStr,
-                        user:user,
-                        time:time,
-                        chapter:chapter
+                        note: noteStr,
+                        user: user,
+                        time: time,
+                        chapter: chapter
                     };
-                warn('data',data);
+                warn('data', data);
                 notes.push(data);
             }
         }
-        var pageNum = parseInt(localStorage.getItem('pageNum'));
+        var pageNum = parseInt((localStorage.getItem('pageNum')));
         pageNum = pageNum ? (pageNum + 1) : 2;
         warn(pageNum);
-        if(flag && pageNum < 5){
-            localStorage.setItem('notes',JSON.stringify(notes));
-            localStorage.setItem('pageNum',pageNum);
+        if (flag && !isPause) {
+            localStorage.setItem('notes', JSON.stringify(notes));
+            localStorage.setItem('pageNum', pageNum);
             this.src = baseUrl + pageNum;
         }
+    }
 
-    };
+    $iframe[0].onload = iframeLoadHandler;
+
+    $collectBtn.click(function(){
+        isPause = true;
+        localStorage.setItem('collectPause',isPause);
+    });
     $btnContainer.append($collectBtn);
     $('body').append($iframe);
     $iframe[0].src = baseUrl + pageNum;
