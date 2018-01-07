3583532042ab485838241ed62048ea17c8d49481
YeomanYe <ming13821007683@gmail.com>
2018-01-07 23:57:57 +0800

    update link-blank

--- link-blank.js
+++ link-blank.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Google、Github超链接从空白标签打开
 // @namespace    https://github.com/yeomanye
-// @version      0.3
+// @version      0.3.1
 // @description  点击超链接从新标签页打开，支持google、github
 // @author       Ming Ye
 // @match        https://www.google.com
 // @match        https://github.com
-// @include      https://github.com*
+// @include      https://github.com/*
 // @include      https://www.google*
 // @grant        none
 // ==/UserScript==
