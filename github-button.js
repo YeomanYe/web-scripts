220e08d5e96bf139c8c7c4938d16c009dfe38007
YeomanYe <ming13821007683@gmail.com>
2018-01-28 14:06:12 +0800

    update myDebugger and github-btn

--- github-button.js
+++ github-button.js
@@ -5,7 +5,8 @@
 // @description  Github下载和复制按钮
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
-// @require      https://greasyfork.org/scripts/34143-debug/code/debug.js?version=245345
+// @require      https://greasyfork.org/scripts/34143-debug/code/debug.js?version=245573
+// @require      https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js
 // @author       Ming Ye
 // @match        https://github.com
 // @include      https://github.com/*/*
@@ -16,36 +17,57 @@ var inline_src = (<><![CDATA[
 /* jshint ignore:end */
     /* jshint esnext: false */
     /* jshint esversion: 6 */
+    debugger;
+    myDebugger.debugD = false;
     let mouseOverHandler = (evt) => {
         // debugTrue();
         let elem = evt.currentTarget,
             aElm = elem.querySelector('.fileDownLink');
         aElm.style.display = 'inline-block';
-    }
+    };
 
     let mouseOutHandler = (evt) => {
         // debugTrue();
         let elem = evt.currentTarget,
             aElm = elem.querySelector('.fileDownLink');
         aElm.style.display = 'none';
+    };
+
+    let linkClick = (evt) => {
+        let elem = evt.currentTarget;
+        var link = document.createElement('a');
+        link.setAttribute('href',elem.getAttribute('download-url'));
+        link.setAttribute('download',elem.getAttribute('filename'));
+        link.click();
     }
     
     let log = myDebugger.consoleFactory("github-btn","log",null,true);
-    let debugTrue = myDebugger.debugTrue();
-    log('github-btn');
+    let debugTrue = myDebugger.debugTrue();    
     let nodeList = document.querySelectorAll('.octicon.octicon-file');
+    debugTrue();
+    let origin = location.origin,
+        href = location.href,
+        path = href.replace(origin,'');
+    if(path.indexOf('tree')<0)
+        path += '/tree/master/';
+    path = path.replace('tree','raw');
     for(let i=0,len=nodeList.length;i<len;i++){
         let trElm = nodeList[i].parentNode.parentNode,
             cntElm = trElm.querySelector('.content'),
+            cntA = cntElm.querySelector('a'),
+            fileName = cntA.innerText,
             aElm = document.createElement('a');
         aElm.innerText = '下载';
         aElm.className = 'fileDownLink';
         aElm.style.cursor = 'pointer';
         aElm.style.display = 'none';
+        aElm.setAttribute('download-url',path+fileName);
+        aElm.setAttribute('filename',fileName);
         cntElm.appendChild(aElm);
         log.logObj('tr',trElm);
         trElm.onmouseover=mouseOverHandler;
         trElm.onmouseout=mouseOutHandler;
+        aElm.onclick = linkClick;
     }
     
 
