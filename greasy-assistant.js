7075e8baf4103fee34ec570a1143c78d2e9145df
YeomanYe <ming13821007683@gmail.com>
2018-02-01 23:38:10 +0800

    update greasy-assistant

--- greasy-assistant.js
+++ greasy-assistant.js
@@ -7,7 +7,7 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js
 // @require      https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js
 // @author       Ming Ye
-// @match        https://greasyfork.org/*/users/
+// @match        https://greasyfork.org/*/users/*
 // @grant        none
 // ==/UserScript==
 
@@ -16,5 +16,18 @@
     myDebugger.debugD = true;
     var log = myDebugger.consoleFactory("greasy-assistant","log",null);
     var debugTrue = myDebugger.debugTrue; 
-    // Your code here...
+    log.logObj('$',$);
+    var $as = $('article h2 a');
+    var urlMap = {};
+    var href = location.href;
+    $as.each(function(i,a){
+        var $a = $as.eq(i);
+        var key = $a.text(),aHref = $a.attr('href');
+        var tmpArr = aHref.split('/');
+        tmpArr = tmpArr[tmpArr.length-1].split('-');
+        var val = aHref.replace(tmpArr[1],'').replace('users','scripts');
+        val = href+val.substr(0,val.length-1) + '/versions/new';
+        urlMap[key] = val;
+    });
+    log.logObj('urlMap',urlMap);
 })();
